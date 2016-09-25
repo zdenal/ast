d095a1b269d6c6440695d0423aa431f9ba4de93c
Zdenal <nevralaz@gmail.com>
2016-09-25 22:18:44 +0200

    First working version.

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,146 @@
 #![feature(plugin, custom_derive)]
+#![feature(default_type_parameter_fallback)]
 #![plugin(serde_macros)]
 
 extern crate serde;
 extern crate serde_json;
 
-// '{and: ["some.value","some.value"], not: "true"}'
+use serde::{Deserialize, Deserializer};
+use serde::de::{SeqVisitor, MapVisitor, Visitor, Error};
 
 #[derive(Deserialize, Debug)]
-struct And {
-  //#[serde(deserialize_with="deserialize_operator")]
-  and: Vec<Code>,
-  not: bool
+struct AndExpression {
+	children: Vec<Expression>,
+	not: bool
 }
 
 #[derive(Deserialize, Debug)]
-struct Or {
-  or: Vec<String>,
-  not: bool
+struct OrExpression {
+	children: Vec<Expression>,
+	not: bool
 }
 
 #[derive(Deserialize, Debug)]
-struct Code(String);
+struct CodeExpression(String);
 
-#[derive(Deserialize, Debug)]
-pub enum Operator {
+#[derive(Debug)]
+enum Expression {
+	And(AndExpression),
+	Or(OrExpression),
+	Code(CodeExpression)
+}
+
+#[derive(Debug)]
+enum Key {
   And,
   Or,
-  Code
+  Not
 }
 
-//fn deserialize_operator<D>(de: &mut D) -> Result<Operator, D::Error>
-        //where D: serde::Deserializer
-    //{
-        //let deser_result: serde_json::Value = try!(serde::Deserialize::deserialize(de));
-        //match deser_result {
-            //serde_json::Value::String(ref s) if &*s == "and" => Ok(Code),
-            //serde_json::Value::String(ref s) if &*s == "or" => Ok(Operator::Or),
-            //serde_json::Value::String(ref s) => Ok(Operator::Code),
-            //_ => Err(serde::de::Error::custom("Unexpected value")),
-        //}
-    //}
+struct ExpressionVec(Vec<Expression>);
+
+impl Deserialize for Key {
+  fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
+    where D: Deserializer,
+  {
+    let value = try!(serde_json::Value::deserialize(deserializer));
+    let key = value.as_str();
+
+    match key {
+      Some("and") => Ok(Key::And),
+      Some("or") => Ok(Key::Or),
+      Some("not") => Ok(Key::Not),
+      _ => Err(D::Error::unknown_field("key"))
+    }
+  }
+}
+
+impl Deserialize for ExpressionVec {
+  fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
+    where D: Deserializer,
+  {
+		struct ExpressionVecVisitor;
+
+    impl Visitor for ExpressionVecVisitor {
+      type Value = ExpressionVec;
+
+      fn visit_seq<V>(&mut self, mut visitor: V) -> Result<Self::Value, V::Error>
+        where V: SeqVisitor
+        {
+          let mut expressions: Vec<Expression> = Vec::new();
+
+          while let Some(expression) = try!(visitor.visit::<Expression>()) {
+            expressions.push(expression);
+          }
+
+          try!(visitor.end());
+
+          Ok(ExpressionVec(expressions))
+        }
+    }
+
+    deserializer.deserialize(ExpressionVecVisitor)
+  }
+}
+
+impl Deserialize for Expression {
+	fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
+		where D: Deserializer,
+	{
+		struct ExpressionVisitor;
+
+    impl Visitor for ExpressionVisitor {
+      type Value = Expression;
+
+      fn visit_str<E>(&mut self, value: &str) -> Result<Self::Value, E>
+        where E: Error
+        {
+          return Ok(Expression::Code(CodeExpression(value.to_string())));
+        }
+
+      fn visit_map<V>(&mut self, mut visitor: V) -> Result<Self::Value, V::Error>
+        where V: MapVisitor
+        {
+          let mut operator = Key::Not;
+          let mut expression_vec = ExpressionVec(Vec::<Expression>::new());
+          let mut not: bool = false;
+
+          while let Some(key) = try!(visitor.visit_key::<Key>()) {
+            match key {
+              Key::Not => {
+                not = try!(visitor.visit_value::<bool>());
+              },
+              Key::And => {
+                operator = Key::And;
+                expression_vec = try!(visitor.visit_value::<ExpressionVec>());
+              },
+              Key::Or => {
+                operator = Key::Or;
+                expression_vec = try!(visitor.visit_value::<ExpressionVec>());
+              }
+            }
+          }
+
+          try!(visitor.end());
+
+          let ExpressionVec(children) = expression_vec;
+
+          return match operator {
+            Key::And => Ok(Expression::And(AndExpression {children: children, not: not})),
+            Key::Or => Ok(Expression::Or(OrExpression {children: children, not: not})),
+            _ => Err(Error::unknown_field("unkonwn field"))
+          }
+        }
+    }
+
+		deserializer.deserialize_map(ExpressionVisitor)
+	}
+}
 
 fn main() {
-    let expression = "{\"and\":[\"some.value\",\"some.value\"], \"not\": \"true\"}";
+    let expression = "{\"and\":[\"some.value\",\"some.value\", {\"or\":[\"some.value\",\"some.value\"]}], \"not\": \"true\"}";
 
-    let deserialized: And = serde_json::from_str(&expression).unwrap();
+    let deserialized: Expression = serde_json::from_str(&expression).unwrap();
 
     println!("Origin expression: {:?}", deserialized);
 }
